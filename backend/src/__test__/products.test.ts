794b3099183314f5009f1d0fb0e79e9184bdd5be
Franklin Rodriguez <franklinserif@gmail.com>
2022-08-20 20:36:26 -0400

    implementing testing

---
+++ backend/src/__test__/products.test.ts
@@ -0,0 +1,9 @@
+describe("task", () => {
+  describe("get task route", () => {
+    describe("given the product does not exit", () => {
+      it("should return a 404", () => {
+        expect(true).toBe(true);
+      });
+    });
+  });
+});
