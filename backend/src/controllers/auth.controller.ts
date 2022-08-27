6a4ba4b3a2c7789893e77a41379e6a9917cb8f75
Franklin Rodriguez <franklinserif@gmail.com>
2022-08-27 17:03:04 -0400

    add auth.controller

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,31 @@
+/**
+ * this module contains all controllers related
+ * to authtentication
+ * @module controllers/auth
+ */
+
+import { Request, Response, NextFunction } from "express";
+import AuthService from "@/services/auth.service";
+import { IUser } from "../app.type";
+
+const service = new AuthService();
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+export function loginController(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { user } = req;
+
+    res.json(service.signToken(user as IUser));
+  } catch (error) {
+    next(error);
+  }
+}
