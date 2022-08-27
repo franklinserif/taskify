4432812838fb28edd16541260a14a8afbcb4bc0a
Franklin Rodriguez <franklinserif@gmail.com>
2022-08-27 13:30:53 -0400

    add auth service getUser

---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,39 @@
+/**
+ * It contains all the classes and it's methods
+ * for manage all the table information related to the user
+ * @module services/auth
+ */
+
+import boom from "@hapi/boom";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import config from "../config";
+import service from "./user.service";
+
+/**
+ * This class define all methos for manage
+ * authentication
+ */
+class AuthService {
+  /**
+   * It will verify if the user exist and he's password
+   * @async
+   * @param {string} email
+   * @param {string} password
+   * @returns {Promise<Object>}
+   */
+  async getUser(email: string, password: string) {
+    const user = await service.findByEmail(email);
+
+    if (!user) throw boom.unauthorized();
+
+    const isMath = bcrypt.compare(password, user.password);
+
+    if (!isMath) throw boom.unauthorized();
+
+    const { password: ps, ...userWithoutPassword } = user;
+
+    return userWithoutPassword;
+  }
+}
