ce2b688c33f48e77f01bcc434093cf66f9784317
Franklin Rodriguez <franklinserif@gmail.com>
2022-09-01 17:27:26 -0400

    jwt strategy

--- backend/src/utils/strategies/jwt.strategy.ts
+++ backend/src/utils/strategies/jwt.strategy.ts
@@ -4,3 +4,24 @@
  */
 
 import { Strategy, ExtractJwt } from "passport-jwt";
+
+import config from "../../config";
+
+/**
+ * @constant
+ */
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secrectOrKey: config.jwtSecret,
+};
+
+/**
+ * JWT strategy for user in the middleware
+ * @type {Object} JWT strategy
+ * @constant
+ */
+const jwtStrategy = new Strategy(options, (payload, done) => {
+  done(null, payload);
+});
+
+export default jwtStrategy;
---
+++ backend/src/utils/strategies/local.strategy.ts
@@ -0,0 +1,5 @@
+/**
+ * This module contains the Local strategy auth
+ * @module utils/strategies/local
+ */
+import { Strategy } from "passport-local";
