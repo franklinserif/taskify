8c52e3a1d145ad0d76eff094246c4931909569ee
Franklin Rodriguez <franklinserif@gmail.com>
2022-09-11 08:20:31 -0400

    add swagger main settings
      - add swagger doc setting file
      - add schemas on swagger setting file

---
+++ backend/src/docs/swagger.ts
@@ -0,0 +1,107 @@
+import swaggerJSDoc, { OAS3Definition, OAS3Options } from "swagger-jsdoc";
+
+const swaggerDefinition: OAS3Definition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Taskify API documentation",
+    version: "1.0.0",
+  },
+  server: [
+    {
+      url: "http://localhost:3000",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        schema: "bearer",
+      },
+    },
+    schemas: {
+      createUserSchema: {
+        type: "object",
+        required: ["firstName", "lastName", "password", "email"],
+        properties: {
+          firstName: {
+            type: "string",
+          },
+          lastName: {
+            type: "string",
+          },
+          password: {
+            type: "string",
+          },
+          email: {
+            type: "string",
+          },
+        },
+      },
+      updateUserSchema: {
+        type: "object",
+        properties: {
+          firstName: {
+            type: "string",
+          },
+          lastName: {
+            type: "string",
+          },
+          email: {
+            type: "string",
+          },
+        },
+      },
+      getUserByIdSchema: {
+        type: "object",
+        properties: {
+          id: {
+            type: "string",
+          },
+        },
+      },
+      confirmCodeSchema: {
+        type: "object",
+        required: ["code", "email"],
+        properties: {
+          code: {
+            type: "number",
+          },
+          email: {
+            type: "string",
+          },
+        },
+      },
+      createCodeUserSchema: {
+        type: "object",
+        required: ["email"],
+        properties: {
+          email: {
+            type: "string",
+          },
+        },
+      },
+      changeUserPasswordSchema: {
+        type: "object",
+        required: ["email", "code", "newPassword"],
+        properties: {
+          email: {
+            type: "string",
+          },
+          code: {
+            type: "number",
+          },
+          newPassword: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+};
+
+const swaggerOptions: OAS3Options = {
+  swaggerDefinition,
+  apis: ["./src/routes/*.ts"],
+};
+
+export default swaggerJSDoc(swaggerOptions);
